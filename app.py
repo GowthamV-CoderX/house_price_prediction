d58e4f1c546873108a15482c779a6ae35baf0048
GowthamV-Coderx <gowthamvudumu007@gmail.com>
2026-07-11 16:11:03 +0530

    solved input edge cases

--- app.py
+++ app.py
@@ -12,22 +12,136 @@ def index():
 
 @app.route('/predictprice',methods=["POST"])
 def predictprice():
-    area = float(request.form["area"])
-    bedrooms = int(request.form["bedrooms"])
-    bathrooms = int(request.form["bathrooms"])
-    stories = int(request.form["stories"])
+
+    errors = []
+
+
+    # Area validation
+    area = request.form["area"]
+
+    if area == "":
+        errors.append("Area is required.")
+
+    else:
+        try:
+            area = float(area)
+
+            if area <= 0:
+                errors.append("Area must be greater than 0.")
+            if area >= 10000:
+                errors.append("Area must be in range of 0 - 10,000 Sq.ft")
+
+        except ValueError:
+            errors.append("Area must contain only numbers.")
+
+
+
+    # Bedrooms validation
+    bedrooms = request.form["bedrooms"]
+
+    if bedrooms == "":
+        errors.append("Bedrooms is required.")
+
+    else:
+        try:
+            bedrooms = int(bedrooms)
+
+            if bedrooms <= 0:
+                errors.append("Bedrooms must be greater than 0.")
+            if bedrooms >= 20:
+                errors.append("Bedrooms must be in range of 0 - 20")
+            if type(bedrooms) == float:
+                errors.append("Bedrooms cannot be decimal must be in integer value")
+
+        except ValueError:
+            errors.append("Bedrooms must be an integer.")
+
+
+
+    # Bathrooms validation
+    bathrooms = request.form["bathrooms"]
+
+    if bathrooms == "":
+        errors.append("Bathrooms is required.")
+
+    else:
+        try:
+            bathrooms = int(bathrooms)
+
+            if bathrooms <= 0:
+                errors.append("Bathrooms must be greater than 0.")
+            
+            if bathrooms > 20:
+                errors.append("Bathrooms must be in range of 0 - 20")
+            
+            if type(bathrooms) == float:
+                errors.append("bathrooms cannot be decimal must be in integer value")
+
+        except ValueError:
+            errors.append("Bathrooms must be an integer.")
+
+
+
+    # Stories validation
+    stories = request.form["stories"]
+
+    if stories == "":
+        errors.append("Stories is required.")
+    else:
+        try:
+            stories = int(stories)
+            if stories < 1 or stories > 10:
+                errors.append("Stories must be between 1 and 4.")
+            if type(stories) == float:
+                errors.append("Stories cannot be decimal must be in integer value")
+        except ValueError:
+            errors.append("Stories must be an integer.")
+
+    # Parking validation
+    parking = request.form["parking"]
+    if parking == "":
+        errors.append("Parking is required.")
+    else:
+        try:
+            parking = int(parking)
+            if parking < 0 or parking > 5:
+                errors.append("Parking must be between 0 and 3.")
+            if type(parking) == float:
+                errors.append("Parking cannot be decimal must be in integer value")
+        except ValueError:
+            errors.append("Parking must be an integer.")
+
+    # If errors exist
+    if errors:
+
+        return render_template(
+            "error.html",
+            errors=errors
+        )
+
+
+
+    # remaining categorical values
 
     mainroad = int(request.form["mainroad"])
     guestroom = int(request.form["guestroom"])
     basement = int(request.form["basement"])
     hotwaterheating = int(request.form["hotwaterheating"])
     airconditioning = int(request.form["airconditioning"])
-
-    parking = int(request.form["parking"])
     prefarea = int(request.form["prefarea"])
+    furnishing_status = request.form["furnishingstatus"]
+    if furnishing_status == "furnished":
+        furnished = 1
+        unfurnished = 0
 
-    furnished = int(request.form["furnished"])
-    unfurnished = int(request.form["unfurnished"])
+    elif furnishing_status == "unfurnished":
+        furnished = 0
+        unfurnished = 1
+
+    else:  # Semi-Furnished
+        furnished = 0
+        unfurnished = 0
+
 
 
     features = [[
@@ -49,7 +163,7 @@ def predictprice():
 
     prediction = linear_regression_model.predict(features)
 
-    price = round(prediction[0], 2)
+    price = round(prediction[0],2)
 
 
     return f"Predicted House Price: ₹ {price}"
